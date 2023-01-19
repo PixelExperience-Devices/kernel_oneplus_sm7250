dc9f52b40dcb096e35912a4c3f1a4f00d2a8806d
jabashque <jabashque@gmail.com>
2023-01-18 17:08:20 -0800

    techpack: display: Set AoD high/low mode and dimlayer based on bl_level
    Let's use a combination of the AoD high/low mode and dimlayer to make
    LP1's perceived screen brightness match the panel's brightness while in
    ON mode.
    
    Change-Id: If396865ed38abd720ee8f2ed638d7b35ea9ef55f

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -226,6 +226,7 @@ struct dsi_panel_oplus_privite {
 	bool low_light_adjust_gamma_support;
 	bool low_light_gamma_is_adjusted;
 	u32 low_light_adjust_gamma_level;
+	u32 aod_low_brightness_threshold;
 };
 #endif /* OPLUS_BUG_STABILITY */
 
@@ -286,6 +287,10 @@ struct dsi_panel {
 	int ba_count;
 	struct oplus_brightness_alpha *dc_ba_seq;
 	int dc_ba_count;
+	struct oplus_brightness_alpha *aod_high_ba_seq;
+	int aod_high_ba_count;
+	struct oplus_brightness_alpha *aod_low_ba_seq;
+	int aod_low_ba_count;
 
 	struct dsi_panel_oplus_privite oplus_priv;
 	int panel_id2;
